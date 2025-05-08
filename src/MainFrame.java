9c451bbbcc5a75ac2bf388ba4ef1cf457383fe77
Adam <adaamloaay@gmail.com>
2025-05-08 23:22:52 +0300

    Frame Init

---
+++ src/MainFrame.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class MainFrame extends JFrame {
+    public MainFrame(){
+        setTitle("Visual Practical Exam");
+        setSize(720, 690);
+        setResizable(false);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+}
